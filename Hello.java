2bfa6be6061378a1086a3d025f1fec00ce4022d9
Michaela <143545267+mpartsch01@users.noreply.github.com>
2025-01-06 19:53:47 -0500

    Update Hello.java
    java syntax error

--- Hello.java
+++ Hello.java
@@ -4,5 +4,6 @@ public class Hello
     {
         System.out.println("Hello, world! From: Java :]");
         System.out.println("Goodbye, world! From: Java :[");
+        System.out.println("This is invalid Java.)
     }
 }
